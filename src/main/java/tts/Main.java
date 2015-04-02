e90f315cc755e0fcfa9cf59bb98b62d9ac0fa56d
Yann Guidez <gy203832@etu.nice.fr>
2015-04-02 22:15:26 +0200

    questions ajoutées

---
+++ src/main/java/tts/Main.java
@@ -0,0 +1,13 @@
+package main.java.tts;
+
+import moteur.Question;
+
+public class Main {
+	public static void main(String[] args){
+		Question q1 = new Question("Gotye - Somebody That I Used to Know","Georges Brassens - L'Auvergnat","Michel Sardou - Les lacs du Conemara","Kendji Girac - Andalouse","Kendji Girac - Andalouse.mp3");
+		Question q2 = new Question("Wu Tang Clan - Protect Ya Neck","Nirvana - Smells Like Teen Spirit","Eminem - Lose Yourself","Carly Rae Jepsen - Call Me Maybe","Carly Rae Jepsen - Call Me Maybe.mp3");
+		Question q3 = new Question("Les Enfoirés - Toute la vie","Simon and Garfunkel - The Sound of Silence","Led Zeppelin - Stairway To Heaven","Rihanna - Diamonds","Rihanna - Diamonds.mp3");
+		Question q4 = new Question("Snoop Dogg - What's My Name","Francis Cabrel - Sarbacane","Johnny Hallyday - Allumez le Feu","Soprano - Fresh Prince","Soprano - Fresh Prince.mp3");
+		
+	}
+}
