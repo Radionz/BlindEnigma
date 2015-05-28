bcdf9407e00e19de7ff45f4fe00594ab439f49a0
Manuel Pavone <pavone.pro@gmail.com>
2015-05-28 12:04:03 +0200

    modifs 6

--- src/jeu/Regles.java
+++ src/jeu/Regles.java
@@ -29,17 +29,17 @@ public class Regles extends FenetreAbstraite{
 
     // renvoie le fichier wave contenant le message d'accueil
     protected  String wavAccueil() {
-        return "../ressources/sons/accueilSon.wav";
+        return "../ressources/tts/regles.mp3";
     }
 
     // renvoie le fichier wave contenant la règle du jeu
     protected  String wavRegleJeu() {
-        return "../ressources/sons/aideF1.wav";
+        return "../ressources/tts/regles.mp3";
     }
 
     // renvoie le fichier wave contenant la règle du jeu
     protected  String wavAide() {
-        return "../ressources/sons/aide.wav";
+        return "../ressources/tts/regles.mp3";
     }
 
     // définition de la méthode abstraite "init()"
@@ -49,9 +49,9 @@ public class Regles extends FenetreAbstraite{
 
         // premier label
         // ce label est géré par les préférences (cf méthode changeColor)
-        String text = "Pour jouer, branchez les manettes à votre ordinateur. Quand l'ordinateur les aura reconues appuyez sur le bouton jouer du menu principal.\n" +
-                "  Vous pouvez jouer jusqu'à quatre joueurs, chaque joueur devra appuyer UNE fois sur le bouton rouge pour dire qu'il participe, puis encore une fois pour lancer la partie.\n" +
-                "  Pour répondre aux questions utiliser 4 petits boutons";
+        String text = "Pour jouer, branchez les manettes a votre ordinateur. Quand l'ordinateur les aura reconues appuyez sur le bouton jouer du menu principal.\n" +
+                "Vous pouvez jouer jusqu'a quatre joueurs, chaque joueur devra appuyer UNE fois sur le bouton rouge pour dire qu'il participe, puis encore une fois pour lancer la partie.\n" +
+                "Pour repondre aux questions utiliser 4 petits boutons";
         lb1 = new JTextArea (text);
         lb1.setLineWrap(true);
         lb1.setEditable(false);
